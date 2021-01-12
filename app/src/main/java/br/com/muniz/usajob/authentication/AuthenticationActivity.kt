fae43910c221e0fac148206e3ad7d55ca653ddec
Lucas Muniz <lucasmuniz@id.uff.br>
2021-01-11 21:48:59 -0300

    Add firebase authentication support

---
+++ app/src/main/java/br/com/muniz/usajob/authentication/AuthenticationActivity.kt
@@ -0,0 +1,67 @@
+package br.com.muniz.usajob.authentication
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import br.com.muniz.usajob.R
+import br.com.muniz.usajob.databinding.ActivityAuthenticationBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import timber.log.Timber
+
+class AuthenticationActivity : AppCompatActivity() {
+
+    companion object {
+        const val RC_SIGN_IN = 1001
+    }
+
+    private lateinit var binding: ActivityAuthenticationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+
+        binding.authenticationLogin.setOnClickListener {
+            launchSignInFlow()
+        }
+    }
+
+    private fun launchSignInFlow() {
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+}
