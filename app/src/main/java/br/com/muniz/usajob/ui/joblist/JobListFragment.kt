9716c308ab3cdcc6152a8b93b9fe742d9d611046
Lucas Muniz <lucasmuniz@id.uff.br>
2021-01-13 00:56:21 -0300

    Add joblist fragment and viewmodel

--- app/src/main/java/br/com/muniz/usajob/ui/joblist/JobListFragment.kt
+++ app/src/main/java/br/com/muniz/usajob/ui/joblist/JobListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import br.com.muniz.usajob.R
 import br.com.muniz.usajob.base.BaseFragment
 import br.com.muniz.usajob.databinding.FragmentJobListBinding
@@ -15,6 +17,15 @@ import br.com.muniz.usajob.databinding.FragmentJobListBinding
 class JobListFragment : BaseFragment() {
 
     private lateinit var binding: FragmentJobListBinding
+
+    private val _viewModel: JobListViewModel by lazy {
+        ViewModelProvider(this,
+            JobListViewModel.Factory(
+                requireActivity().application
+            )
+        ).get(JobListViewModel::class.java)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +39,13 @@ class JobListFragment : BaseFragment() {
                 R.layout.fragment_job_list, container, false
             )
 
+        _viewModel.resultJob.observe(viewLifecycleOwner, { newJob ->
+            binding.content.text = "Job size: ${newJob.size}"
+        })
+
+        binding.fragmentJobGetJob.setOnClickListener {  }
         return binding.root
     }
+
+
 }
---
+++ app/src/main/java/br/com/muniz/usajob/ui/joblist/JobListViewModel.kt
@@ -0,0 +1,70 @@
+package br.com.muniz.usajob.ui.joblist
+
+import android.app.Application
+import androidx.lifecycle.*
+import br.com.muniz.usajob.data.Job
+import br.com.muniz.usajob.data.local.getDatabase
+import br.com.muniz.usajob.data.remote.Network
+import br.com.muniz.usajob.data.repository.JobRepository
+import br.com.muniz.usajob.utils.DataState
+import br.com.muniz.usajob.utils.asDomainModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+class JobListViewModel(application: Application) : AndroidViewModel(application){
+
+    private var _resultJob = MutableLiveData<List<Job>>()
+    val resultJob: LiveData<List<Job>> = _resultJob
+
+    private var _statusLoading = MutableLiveData<Boolean>()
+    val statusLoading: LiveData<Boolean> = _statusLoading
+
+    private val jobRepository: JobRepository by lazy {
+        val database = getDatabase(getApplication())
+        JobRepository(database)
+    }
+
+    init {
+        viewModelScope.launch {
+            jobRepository.jobLocal.map {
+                it.asDomainModel()
+            }.collect {
+                if(it.isNotEmpty()) {
+                    _resultJob.postValue(it)
+                } else {
+                    refreshJobs()
+                }
+            }
+        }
+    }
+
+    fun refreshJobs() {
+        viewModelScope.launch {
+            jobRepository.refreshJobs().collect { state ->
+                when(state) {
+                    DataState.Loading -> {
+                        _statusLoading.value = true
+                    }
+                    DataState.Error -> {
+                        _statusLoading.value = false
+                    }
+                    DataState.Success -> {
+                        _statusLoading.value = false
+                    }
+                }
+            }
+        }
+    }
+
+    class Factory(private val application: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(JobListViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return JobListViewModel(application) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
